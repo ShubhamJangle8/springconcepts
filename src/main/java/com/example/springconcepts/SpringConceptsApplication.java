74c1d912e961b5763820132c3497b9734abdfa79
jangle <shubhamjangale0000@gmail.com>
2020-12-08 10:24:16 +0530

    Added : Creating a Spring Project

---
+++ src/main/java/com/example/springconcepts/SpringConceptsApplication.java
@@ -0,0 +1,14 @@
+package com.example.springconcepts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConceptsApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to spring concepts");
+		SpringApplication.run(SpringConceptsApplication.class, args);
+	}
+
+}

9f7c3b6b32a4af3aefb38ece91240ee0a6bf08d8
jangle <shubhamjangale0000@gmail.com>
2020-12-08 18:44:29 +0530

    Added : Added the logging messages using slf4j

--- src/main/java/com/example/springconcepts/SpringConceptsApplication.java
+++ src/main/java/com/example/springconcepts/SpringConceptsApplication.java
@@ -1,14 +1,23 @@
 package com.example.springconcepts;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.example.springconcepts.component.DemoBean;
 
 @SpringBootApplication
 public class SpringConceptsApplication {
-
+	public static final Logger logger = LoggerFactory.getLogger(SpringConceptsApplication.class);
+	
 	public static void main(String[] args) {
-		System.out.println("Welcome to spring concepts");
-		SpringApplication.run(SpringConceptsApplication.class, args);
+		logger.debug("Welcome to spring concepts");
+		ApplicationContext context = SpringApplication.run(SpringConceptsApplication.class, args);
+		DemoBean demoBean = context.getBean(DemoBean.class);
+		logger.info("DemoBean = " + demoBean.toString());
 	}
 
 }
+		
